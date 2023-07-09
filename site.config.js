4d82be8e43cde0d90ae16f738a93241b94dcf438
jieun <najee0610@naver.com>
2023-07-09 15:09:22 +0900

    블로그 기본 설정

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "jini77u",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I always trying",
+    email: "jul061055@gmail.com",
+    // linkedin: "morethanmin",
+    github: "ijieun",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `gachiisada`,
+      href: "https://github.com/mju-likelion/gachiisda-frontend",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jini77u-log",
+    description: "welcome to jini77u-log!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
